f07283d3bccef3ff28580060626a066087e218ff
Martijn Beenker <Martijn.Beenker@gmail.com>
2023-10-29 10:46:13 +0100

    Change character range to also include lowercase letters

--- load-balancing/_functions.ps1
+++ load-balancing/_functions.ps1
@@ -1,7 +1,7 @@
 function New-VmPassword {
     $vmPassword = ""
     foreach ($i in 1..32) {
-        $vmPassword += [char[]](30..47 + 41..80 + 123..126 | Get-Random -Count 1)
+        $vmPassword += [char[]](33..47 + 41..80 + 97..122 + 123..126 | Get-Random -Count 1)
     }
     return $vmPassword
 }
