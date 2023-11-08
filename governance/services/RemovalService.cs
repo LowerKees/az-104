c0f365067703251f9d5e46f949b237b55a09e847
Martijn Beenker <Martijn.Beenker@gmail.com>
2023-11-08 17:34:55 +0100

    first setup of dotnet based function app

---
+++ governance/services/RemovalService.cs
@@ -0,0 +1,17 @@
+import 
+
+namespace governance
+{
+    public class RemovalService
+    {
+        public RemovalService(string subscription_id)
+        {
+            this.subscription_id = subscription_id;
+        }
+
+        public void Run()
+        {
+             
+        }
+    }
+}
